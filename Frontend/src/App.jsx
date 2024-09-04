8fe87014c71b5e63c85c409e0cbacf7d36ddef4e
NOTBOOSTER <iamnotbooster@gmail.com>
2024-09-04 21:17:19 +0530

    test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      test
-    </>
+    <div>Whatsapp Clone</div>
   )
 }
 
